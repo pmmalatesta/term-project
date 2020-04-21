5aa1732b32c763cfb3df550a2fc76e6723b5470a
m214134 <m214134@WK2TK72202PJMID.academy.usna.edu>
2020-04-21 14:28:41 -0400

    zero cancellation, solved the transient response

--- TP.m
+++ TP.m
@@ -13,8 +13,9 @@ ts=2;
 os=.0432;
 zeta=(-log(os)/pi)/sqrt(1+(log(os)/pi)^2);
 wn=4/(zeta*ts);
-phi=roots([1 2.1*wn 3.4*wn*wn 2.7*wn*wn*wn wn*wn*wn*wn]);
-chareq2=[1 2*wn*zeta wn*wn]
+chareq2des=[1 2*wn*zeta wn*wn];
+zeroRemove=conv([1 3.1321],[1 wn]);
+phi=roots(conv(zeroRemove,chareq2des));
 K=acker(A,B,phi);
 [numCL,denCL]=ss2tf(A-B*K,B,C,D);
 cltf=tf(numCL,denCL);
@@ -24,4 +25,16 @@ x_0=0;
 theta_0=0;
 theta_dot_0=0;
 F=0;%
-sim('PendulumCart_Block.slx', 0:.1:12)
+%%
+sim('PendulumCart_Block.slx', 0:.1:12);
+sim('tpSimul.slx',0:.1:12);
+max(nlpos.Data)
+plot(nlpos.time,nlpos.Data);
+%plot(out.position.time,out.position.Data);
+hold on;
+plot(t,maxy)
+plot(t,min);
+plot(grabbadabbah,grubba);
+plot(t,over);
+%figure(2);
+%plot(nlstates.time,nlstates.Data(:,2));
