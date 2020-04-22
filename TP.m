2268a95a46cab4ff0bbcb300b53183edad3b0ee7
m214134 <m214134@WK2TK72202PJMID.academy.usna.edu>
2020-04-22 12:55:11 -0400

    not sure what I changed, but push it

--- TP.m
+++ TP.m
@@ -32,6 +32,7 @@ cltf=tf(numCL,denCL);%transfer function for cal gain
 calGain=1/dcgain(cltf);
 lineStates=sim('tpSimul.slx',0:.1:12);
 sim('PendulumCart_Block.slx', 0:.1:12);
+pOver=100*(max(nlpos.Data)-1);
 %%
 %Maps generated and saved as pngs deliverable 1
 %If formatting needs to be changed, run this section to generate
@@ -73,10 +74,29 @@ ref=ones(1,13);
 t=0:1:12;
 plot(t,ref,'--k');
 hold on;
-plot(nlpos.time,nlpos.Data,'b');
-plot(lineStates.position.time,lineStates.position.Data,'r');
+plot(nlpos.time,nlpos.Data,'*b');
+plot(lineStates.position.time,lineStates.position.Data,'.-r');
 xlabel("Time(s");
 ylabel("Position (m)");
+legend("Reference Input","Non-linear System", "Linearized System");
 title("I don't know what to put in right now, please think of a good title AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+%%
+%Deliv 4 velocities
+subplot(2,1,1);
+plot(nlstates.time,nlstates.Data(:,1),'b');
+hold on;
+plot(lineStates.states.time,lineStates.states.Data(:,1),'--r');
+xlabel("Time(s");
+ylabel("Angular Velocity (rad/s)");
+title("Pendulum Angular Velocity vs. Time");
+legend("Non-linear System", "Linearized System");
+subplot(2,1,2);
+plot(nlstates.time,nlstates.Data(:,3),'b');
+hold on;
+plot(lineStates.states.time,lineStates.states.Data(:,3),'--r');
+xlabel("Time(s");
+ylabel("Velocity (m/s)");
+title("Cart Velocity vs. Time");
+legend("Non-linear System", "Linearized System");
 %%
 PendulumCartAnim(nlstates.Data(:,4),nlstates.Data(:,2),nlstates.time,l);
